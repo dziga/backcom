272ecef46bfda5bea6260ca3cbab2898c7ee6967
Nikola Milivojevic <n.milivojevic@levi9.com>
2015-02-09 11:20:53 +0100

    default host

--- src/main/java/com/dziga/backcom/api/v1/RestEndpoints.java
+++ src/main/java/com/dziga/backcom/api/v1/RestEndpoints.java
@@ -1,6 +1,6 @@
 package com.dziga.backcom.api.v1;
 
 public class RestEndpoints {
-	public static final String HOST = "www.backcom.com";
+	public static final String HOST = "localhost:8089";
 	public static final String CUSTOMER_LIST = "/v1/customers";
 }
